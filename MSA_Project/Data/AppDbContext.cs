41167cf1bf87f7407b1e3d34eb3e464d8ff97661
Daryle Ji <daryleji.t@gmail.com>
2021-09-17 20:15:17 +1200

    initial commit

---
+++ MSA_Project/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MSA_Project.Modals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSA_Project.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+        
+        public DbSet<Student> Students { get; set; }
+
+
+    }
+}
---
+++ MSA_Project/GraphQL/Students/StudentQueries.cs
@@ -0,0 +1,21 @@
+﻿using HotChocolate;
+using HotChocolate.Types;
+using MSA_Project.Data;
+using MSA_Project.Modals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSA_Project.GraphQL.Students
+{
+    [ExtendObjectType(name: "Query")]
+    public class StudentQueries
+    {
+        public IQueryable<Student> GetStudents([ScopedService] AppDbContext context)
+        {
+            return context.Students;
+        }
+
+    }
+}
