e6e7f7e2362fe688c5c4e69bd3c886852d0a99db
Daryle Ji <daryleji.t@gmail.com>
2021-09-23 05:07:11 +1200

    app changes in front end + commit for backend test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,10 @@
 import React from "react";
-import MenuIcon from "@material-ui/icons/Menu";
-import { AppBar, IconButton, Toolbar } from "@material-ui/core";
-import { Button, Typography } from "@material-ui/core";
-import { SubmitForm } from './components/SubmitForm/SubmitForm';
 import MSAHeader from './components/MSAHeader/MSAHeader';
 import { Footer } from './components/MSAFooter/MSAFooter';
 import { gql } from '@apollo/client';
 import { HomePage } from "./Pages/HomePage";
 import { SubmitPage } from "./Pages/SubmitPage";
 import { Route, Switch } from 'react-router';
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-
-
-export const PROJECT = gql`
-query {
-  Projects {
-    nodes {
-      name
-    }
-  }
-}
-`
 
 function App() {
   return (
