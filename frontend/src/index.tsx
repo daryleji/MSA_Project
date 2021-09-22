48dbf6fdb4676b64f1e897012e26811c49be2c28
Daryle Ji <daryleji.t@gmail.com>
2021-09-23 05:25:33 +1200

    App and index file changes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
 
